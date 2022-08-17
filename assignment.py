9f79abe3ead3993cf87d63b98795b571d6a7cacc
RonnieNoel <noelronnie050@gmail.com>
2022-08-17 21:20:55 +0300

    this is my first python assignment cit day 3

---
+++ assignment.py
@@ -0,0 +1,9 @@
+#String Myname
+my_name = "Ronnie"
+#integer
+my_age = 24
+#float
+my_weight = 60.5
+
+# My name is Ronnie, I am 24 years old and I weigh 60.5kgs
+print("My name is " + my_name + ", I am " + str(my_age) + " years old and I weigh " + str(my_weight) + "kgs")
