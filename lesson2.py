9553cae2d977f54cd9909e8487173a39fdc0ddb1
RonnieNoel <noelronnie050@gmail.com>
2022-08-23 18:00:25 +0300

    classwork

--- lesson2.py
+++ lesson2.py
@@ -1,17 +1,19 @@
-mode=int(input("type 1 for C to F & 2 for F to C :---->"))
+# mode=int(input("type 1 for C to F & 2 for F to C :---->"))
 
-conversion=0
+# conversion=0
 
-if mode==1:
-    temperature=int(input("enter your temperature here:----> "))
-    # formular degrees C to F
-    conversion=(temperature * (9/5))+ 32
-    print("your temperature is "+str(conversion)+" degrees fahrenheit")
+# if mode==1:
+#     temperature=int(input("enter your temperature here:----> "))
+#     # formular degrees C to F
+#     conversion=(temperature * (9/5))+ 32
+#     print("your temperature is "+str(conversion)+" degrees fahrenheit")
 
-elif mode==2:
-    temperature=int(input("enter your temperature here:----> "))
-    #formular degrees F to C
-    conversion=(temperature-32)*(5/9)
-    print("your temperature is "+str(conversion)+" degrees Celicius")
-else:
-    print("Error wrong input")
+# elif mode==2:
+#     temperature=int(input("enter your temperature here:----> "))
+#     #formular degrees F to C
+#     conversion=(temperature-32)*(5/9)
+#     print("your temperature is "+str(conversion)+" degrees Celicius")
+# else:
+#     print("Error wrong input")
+# print("hello")
+# print('hello')
---
+++ lesson3.py
@@ -0,0 +1,71 @@
+# #a simple calculator
+# num1=int(input("enter a first number"))
+# num2=int(input("enter a second number"))
+
+# operator=int(input("enter operand 1-->ADD 2-->SUB 3-->DIV 4-->MUL"))
+# add=1
+# sub=2
+# div=3
+# mul=4
+
+# if add==operator:
+#     print(f"{num1}+{num2}={num1+num2}")
+# elif sub==operator:
+#      print(f"{num1}-{num2}={num1-num2}")
+# elif div==operator:
+#      print(f"{num1}/{num2}={num1/num2}")
+# elif mul==operator:
+#      print(f"{num1}*{num2}={num1*num2}")
+# else:
+#      print("You entered a wrong option")
+# num1=int(input("enter a first number"))
+# num2=int(input("enter a second number"))
+# if num1*num2<=1000:
+#     print(f"{num1}*{num2}={num1*num2}")
+# else:
+#     print(f"{num1}+{num2}={num1+num2}")
+# print('“Name**Is**James”')
+
+# num1=64
+# num2=32
+# print(f"{num1}+{num2}={num1+num2}")
+
+# program
+
+# num=10
+# if num >= 5:
+#     print("the mumber you have is greater than 5")
+# else:
+#     print("the mumber you have is less than 5")
+
+# #summation 
+# num1=1
+# num2=2
+# print(f"{num1}+{num2}={num1+num2}")
+
+
+# #A simple grading calculator that uses if statement and comparision operators 
+
+# number=int(input("enter a number"))
+
+# if number>100:
+#     print("enter a correct percentage to get the grade")
+# elif number>=90:
+#      print("Excellent:A")
+# elif number>=80:
+#      print("Very Good:B")
+# elif number>=70:
+#      print("Good:D")
+# elif number>=60:
+#      print("Fair:E")
+# elif number>=0:
+#      print("You Failed:F")
+# elif number<0:
+#       print("enter a correct percentage to get the grade")
+# var1 = 1
+# var2 = 2
+# var3 = "3"
+
+# print(var1 + var2 + var3)
+var x = float(2.8)
+print(x)
